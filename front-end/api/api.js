367086fc59a1b8e9a578da9b609ebc22ebfb0372
Vitor_R_De_Souza <101133891+Vitor-R-Souza@users.noreply.github.com>
2025-02-15 17:43:33 -0300

    ajuste process.env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://spotfy-jornada-fullstack.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
-
-// console.log(responseSongs.data);
